6a7ee66a2b4f6c734496ebca9d582eca3ffc5182
Pierre Galaup <contact@pierre-galaup.fr>
2016-01-29 20:32:21 +0100

    first commit base

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    
+    private static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var notificationObject = new GameObject("GameManager");
+                _instance = (GameManager)notificationObject.AddComponent(typeof(GameManager));
+            }
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject MainMenuCanvas;
+    public GameObject CreditsCanvas;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void Credits()
+    {
+        MainMenuCanvas.SetActive(false);
+        CreditsCanvas.SetActive(true);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+
+    public void Back()
+    {
+        MainMenuCanvas.SetActive(true);
+        CreditsCanvas.SetActive(false);
+    }
+}
