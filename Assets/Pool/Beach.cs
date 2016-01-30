0aeb95580c1972cff6dafe933b97dc460effb3e9
dimitri-dolle <typenn@hotmail.fr>
2016-01-30 18:10:23 +0100

    Pool add

---
+++ Assets/Pool/Beach.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beach : MonoBehaviour {
+
+
+    [SerializeField]
+    private AudioSource ScreamAudioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.ScreamAudioSource.Play();
+    }
+}
