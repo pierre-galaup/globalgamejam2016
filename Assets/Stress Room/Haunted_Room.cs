1e8a6af499031ba28c3c1c6422d2a04e61bba483
Pierre Galaup <contact@pierre-galaup.fr>
2016-01-30 15:41:57 +0100

    Fixes

---
+++ Assets/Stress Room/Haunted_Room.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Haunted_Room : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource ScreamAudioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.ScreamAudioSource.Play();
+    }
+}
---
+++ Assets/Stress Room/Tunnel_sound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tunnel_sound : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource ScreamAudioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        this.ScreamAudioSource.Play();
+    }
+}
