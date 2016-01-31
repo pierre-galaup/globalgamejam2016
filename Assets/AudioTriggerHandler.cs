4112f336668ff0105159baed83b0113ddebcca25
Nicolas Garcia <nicodu09@gmail.com>
2016-01-31 14:39:34 +0100

    Audio triggers in bedroom. Improve audio trigger script fix

--- Assets/AudioTriggerHandler.cs
+++ Assets/AudioTriggerHandler.cs
@@ -10,13 +10,20 @@ public class AudioTriggerHandler : MonoBehaviour
 
     [SerializeField] private AudioSource audioSource;
 
+    [SerializeField]
+    private bool IsUnique = true;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Player")
             return;
         if (this.Clip != null)
+        {
             this.audioSource.PlayOneShot(this.Clip);
+        }
         if (this.ScriptToRun != null)
             this.ScriptToRun.enabled = true;
+        if (this.IsUnique)
+            this.gameObject.SetActive(false);
     }
 }
